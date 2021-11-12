666a11a950099490ce4a96d1fdc7ac03267ce719
SofienBouriga <sofien.bouriga@gmail.com>
2021-11-12 17:41:28 +0100

    new main file added for sampling images

---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+from CameraClient import CameraClient, ImageType, Command, CameraIntri
+import sys
+import cv2
+import numpy as np
+import os
+import open3d
+
+from datetime import datetime
+#sys.path.append(r"C:\Coding\Python\Programs_Sofien")
+#os.getcwd()
+
+SAVE_PATH = "D:" # todo add path
+
+if __name__ == '__main__':
+
+    camera = CameraClient()
+    save_file = True
+    # camera ip should be modified to actual ip address
+    camera_ip = "192.168.3.226" # todo change IP
+    # always set ip before do anything else
+    if not camera.connect(camera_ip):
+        exit(-1)
+    # get some camera info like intrincis, ip, id, version and image size
+    intri = camera.getCameraIntri()
+    print ("Camera Info: %s" % (camera.getCameraInfo()))
+    print ("Camera ID: %s" % (camera.getCameraId()))
+    print ("Version: %s" % (camera.getCameraVersion()))
+    print ("Color Image Size: %s %s" % (camera.getColorImgSize()))
+    print ("Depth Image Size: %s %s" % (camera.getDepthImgSize()))
+
+    # capture depth image and color image and save them
+    depth = camera.captureDepthImg()
+    color = camera.captureColorImg()
+    # call time
+    now = datetime.now()
+    date= now.strftime("%Y%m%d_%H%M%S")
+
+    if len(depth) == 0 or len(color) == 0:
+        exit(-2)
+
+    if save_file:
+        if not os.path.exists(SAVE_PATH):
+            os.makedirs(SAVE_PATH)
+        cv2.imwrite(SAVE_PATH + "\mechmind_depth" + date + ".png", depth)
+        cv2.imwrite(SAVE_PATH + "\mechmind_color" + date + ".jpg", color)
+    # set some parameters of camera, you can refer to parameters' names in Mech_eye
+
+    # get rgb point cloud. Using open3d to store and visualize the cloud.
+    pcd = camera.captureCloud()
+    open3d.visualization.draw_geometries([pcd])
+    # set some parameters of camera, you can refer to parameters' names in Mech_eye
+    print(camera.setParameter("scan2dExposureMode",0)) # set exposure mode to Timed
+    print(camera.getParameter("scan2dExposureMode"))
+    print(camera.setParameter("scan2dExposureTime",20)) # set exposure time to 20ms
+    print(camera.getParameter("scan2dExposureTime"))
+    exit(0)
